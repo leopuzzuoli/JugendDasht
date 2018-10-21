f59df958bade87d3c9034838857e87364f0b5784
Leonardo Puzzuoli <leonardo@paolos-MacBook-Pro.local>
2018-10-21 12:11:34 +0200

    Prettified

--- instagetter.js
+++ instagetter.js
@@ -5,34 +5,34 @@ let delay = require("delay");
 let images = [];
 
 function dataminer() {
-  (async() => {
-  while (true) {
-    https.get('https://www.instagram.com/explore/tags/jugendhackt/?__a=1', (resp) => {
-      let data = '';
+  (async () => {
+    while (true) {
+      https.get('https://www.instagram.com/explore/tags/jugendhackt/?__a=1', (resp) => {
+        let data = '';
 
-      // A chunk of data has been recieved.
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
-
-      // The whole response has been received. Print out the result.
-      resp.on('end', () => {
-        let jsondata = JSON.parse(data);
-        for (var i = 0; i < 6; i++) {
-          images.push(jsondata.graphql.hashtag.edge_hashtag_to_media.edges[i].node.display_url);
-        }
-        console.log(images);
-        console.log(JSON.stringify(images));
-        fs.writeFile('./website/daten/instafiles.json', JSON.stringify(images), 'utf8', function() {
-          console.log("Toll gemacht");
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+          data += chunk;
         });
-      });
 
-    }).on("error", (err) => {
-      console.log("Error: " + err.message);
-    });
-    await delay(5000);
-  }
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+          let jsondata = JSON.parse(data);
+          for (var i = 0; i < 6; i++) {
+            images.push(jsondata.graphql.hashtag.edge_hashtag_to_media.edges[i].node.display_url);
+          }
+          console.log(images);
+          console.log(JSON.stringify(images));
+          fs.writeFile('./website/daten/instafiles.json', JSON.stringify(images), 'utf8', function() {
+            console.log("Toll gemacht");
+          });
+        });
+
+      }).on("error", (err) => {
+        console.log("Error: " + err.message);
+      });
+      await delay(5000);
+    }
   })();
 }
 
