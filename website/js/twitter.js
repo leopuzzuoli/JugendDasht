1b6e4e425e684b585607442d2250332da1a0d3ab
FLorinus MXMS <Holzbauleiste>
2018-10-21 10:40:45 +0200

    ERSTE TWITTER
    E

--- website/js/twitter.js
+++ website/js/twitter.js
@@ -1,3 +1,20 @@
-twttr.widgets.load(
-  document.getElementById("twitter")
-);
+
+window.onload = (function(){
+	var id = '1053909698641084416'
+	var tweet = document.getElementById("tweet");
+	//var id = tweet.getAttribute("tweetID");
+
+	twttr.widgets.createTweet(
+	id, tweet, 
+	{
+	conversation : 'none',// or all
+	cards: 'hidden',// or visible 
+	linkColor: '#cc0000', // default is blue
+	theme: 'light'// or dark
+})
+
+
+
+
+
+});
